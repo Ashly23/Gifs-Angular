6b95a1a53e5b10b34b4a3a1094b5773345799d4b
Ashly Pineda <pinedaashly23@gmail.com>
2022-04-11 17:00:44 -0600

    first commit

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,18 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef;
+
+  buscar( termino: string )  {
+     
+    console.log(this.txtBuscar);
+  }
+
+}
